ea95c5205a4122a6eb6366a853b0a810f1dc9608
Prabhakaran Ramaswamy <pkaran.ramaswamy@gmail.com>
2025-09-01 10:34:10 +0530

    c

---
+++ AuthorizationCodeGrant/src/main/java/com/example/controller/TokenController.java
@@ -0,0 +1,43 @@
+package com.example.controller;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+	
+	
+	@GetMapping("/gettoken")
+	public Jwt getToken(@AuthenticationPrincipal Jwt jwt) {
+		
+		return jwt;
+	}
+	
+	@PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject") 
+	//@Secured("ROLE_developer")
+    @DeleteMapping(path = "/{id}")
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
+    }
+	
+
+	@PostAuthorize("returnObject.userId == #jwt.subject")
+    @GetMapping(path = "/{id}")
+    public String getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return "5f3fb480-f86c-4514-8d23-ca88d66c6253";
+    }
+
+}
